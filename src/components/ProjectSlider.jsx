d545feb019517809ddc0b31867422fc3e6118a2a
VincentChen6345 <vincent.chen6345@hotmail.com>
2023-01-06 11:15:59 +1100

    carousel slider bug fixed

--- src/components/ProjectSlider.jsx
+++ src/components/ProjectSlider.jsx
@@ -3,28 +3,35 @@ import project1 from "../img/project-1.png";
 import project2 from "../img/project-2.png";
 import project3 from "../img/project-3.png";
 import project4 from "../img/project-4.png";
+import React, { useRef, useEffect } from "react";
 
 export const ProjectSlider = () => {
-  const slides = document.querySelectorAll(".slide");
-  slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
   let curSlide = 0;
-  const maxSlide = slides.length;
+  const slidesRef = useRef();
+  let maxSlides;
   const goToSlide = (slideNumber) => {
+    const slides = slidesRef.current.querySelectorAll(".slide");
+    maxSlides = slides.length;
     slides.forEach(
       (s, i) => (s.style.transform = `translateX(${100 * (i - slideNumber)}%)`)
     );
   };
-  goToSlide(0);
-  const nextSlide = (side) => {
-    if (side === "left") curSlide--;
-    if (side === "right") curSlide++;
+  useEffect(() => {
+    goToSlide(0);
+  }, []);
+  const nextSlide = () => {
+    curSlide++;
+    if (curSlide > maxSlides - 1) curSlide = 0;
+    goToSlide(curSlide);
+  };
+  const prevSlide = () => {
+    curSlide--;
     if (curSlide < 0) curSlide = 3;
-    if (curSlide > maxSlide - 1) curSlide = 0;
     goToSlide(curSlide);
   };
   return (
     <div className="slider-container">
-      <div className="js-slider">
+      <div className="js-slider" ref={slidesRef}>
         <div className="slide slide--1">
           <img src={project1} alt="" className="project-image" />
         </div>
@@ -37,16 +44,10 @@ export const ProjectSlider = () => {
         <div className="slide slide--4">
           <img src={project4} alt="" className="project-image" />
         </div>{" "}
-        <button
-          className="slider__btn slider__btn--left"
-          onClick={() => nextSlide("left")}
-        >
+        <button className="slider__btn slider__btn--left" onClick={prevSlide}>
           &larr;
         </button>
-        <button
-          className="slider__btn slider__btn--right"
-          onClick={() => nextSlide("right")}
-        >
+        <button className="slider__btn slider__btn--right" onClick={nextSlide}>
           &rarr;
         </button>
       </div>
