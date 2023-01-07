5a12d5472eb4685638635997defb1991e051f492
VincentChen6345 <vincent.chen6345@hotmail.com>
2023-01-08 09:26:54 +1100

    carousel bug fixed

--- src/components/ProjectSlider.jsx
+++ src/components/ProjectSlider.jsx
@@ -4,6 +4,7 @@ import project2 from "../img/project-2.png";
 import project3 from "../img/project-3.png";
 import project4 from "../img/project-4.png";
 import React, { useRef, useEffect, useCallback } from "react";
+import ProjectText from "./ProjectText";
 
 export const ProjectSlider = () => {
   let curSlide = 0;
@@ -16,33 +17,53 @@ export const ProjectSlider = () => {
     );
   }, []);
 
-  useEffect(() => {
-    goToSlide(0);
-  }, [goToSlide]);
   const nextSlide = () => {
+    console.log("nextSlide fired");
+
     curSlide++;
-    if (curSlide > 3) curSlide = 0;
+
+    if (curSlide > 3) {
+      curSlide = 0;
+    }
+
     goToSlide(curSlide);
   };
   const prevSlide = () => {
+    console.log("prevSlide fired");
+
     curSlide--;
-    if (curSlide < 0) curSlide = 3;
+
+    if (curSlide < 0) {
+      curSlide = 3;
+    }
+
     goToSlide(curSlide);
   };
+  useEffect(
+    () => {
+      goToSlide(curSlide);
+    },
+    [goToSlide],
+    curSlide
+  );
   return (
     <div className="slider-container">
       <div className="js-slider" ref={slidesRef}>
         <div className="slide slide--1">
           <img src={project1} alt="" className="project-image" />
+          <ProjectText slide={0} />
         </div>
         <div className="slide slide--2">
           <img src={project2} alt="" className="project-image" />
+          <ProjectText slide={1} />
         </div>
         <div className="slide slide--3">
           <img src={project3} alt="" className="project-image" />
+          <ProjectText slide={2} />
         </div>
         <div className="slide slide--4">
           <img src={project4} alt="" className="project-image" />
+          <ProjectText slide={3} />
         </div>{" "}
         <button className="slider__btn slider__btn--left" onClick={prevSlide}>
           &larr;
@@ -50,31 +71,6 @@ export const ProjectSlider = () => {
         <button className="slider__btn slider__btn--right" onClick={nextSlide}>
           &rarr;
         </button>
-        <div className="project-overview hidden">
-          <h3 className="project-title">Bruce Lee Tribute Page</h3>
-          <ul className="language-container">
-            <li className="language">HTML</li>
-            <li className="language">CSS</li>
-          </ul>
-          <p className="paragraph-text">
-            Dedicated to my late idol Bruce Lee, I created this tribute page to
-            practice pure HTML and CSS.
-          </p>
-          <span className="btn-container">
-            <a
-              href="https://tribute-to-bruce-lee.netlify.app/"
-              className="btn btn-visit"
-            >
-              Visit
-            </a>
-            <a
-              href="https://github.com/VincentChen6345/tribute-to-bruce-lee"
-              className="btn btn-code"
-            >
-              Code
-            </a>
-          </span>
-        </div>
       </div>
       <div className="dots"></div>
     </div>
---
+++ src/components/ProjectText.jsx
@@ -0,0 +1,57 @@
+const content = {
+  title: [
+    "Bruce Lee Tribute Page",
+    "Pricing Toggle Button",
+    "Credit Card Validation Form",
+    "Life Advice Generator",
+  ],
+  languages: [
+    ["HTML", "CSS"],
+    ["HTML", "CSS", "JS", "REACT"],
+    ["HTML", "CSS", "JS", "REACT"],
+    ["HTML", "CSS", "JS", "REACT"],
+  ],
+  paragraph: [
+    "   Dedicated to my late idol Bruce Lee, I created this tribute page to practice pure HTML and CSS.",
+    "This is a challenge project from FrontEndMentor.io that I built on my own. The toggle button uses state to change the pricing details of each plan.",
+    "This is a challenge project from FrontEndMentor.io that I built on my own. When all inputs are valid, clicking confirm will print your details on the card images. I prop propagation, useState and string manipulation to build the validation logic.  ",
+    " This is a challenge project from FrontEndMentor.io that I built on my own. A simple life advice generator which fetches data from an api when the button is clicked and renders it. ",
+  ],
+  site: [
+    "https://tribute-to-bruce-lee.netlify.app/",
+    "https://gleeful-bublanina-d35e0e.netlify.app/",
+    "https://interactive-credit-card-form.netlify.app/",
+    "https://zesty-alfajores-911695.netlify.app/",
+  ],
+  code: [
+    "https://github.com/VincentChen6345/tribute-to-bruce-lee",
+    "https://github.com/VincentChen6345/toggle-master",
+    "https://github.com/VincentChen6345/interactive-credit-card-form",
+    "https://github.com/VincentChen6345/advice-generator",
+  ],
+};
+
+const ProjectText = (props) => {
+  return (
+    <div className="project-overview">
+      <h3 className="project-title">{content.title[props.slide]}</h3>
+      <ul className="language-container">
+        {content.languages[props.slide].map((item) => (
+          <li className="language" key={Math.random().toString()}>
+            {item}
+          </li>
+        ))}
+      </ul>
+      <p className="paragraph-text">{content.paragraph[props.slide]}</p>
+      <span className="btn-container">
+        <a href={content.site[props.slide]} className="btn btn-visit">
+          Visit
+        </a>
+        <a href={content.code[props.slide]} className="btn btn-code">
+          Code
+        </a>
+      </span>{" "}
+    </div>
+  );
+};
+export default ProjectText;
